951c23a27abd610e54f8bf711292be145671fb90
migbash <20924663+migbash@users.noreply.github.com>
2024-03-26 12:33:02 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+
+  import WidgetAdEngine from '@betarena/ad-engine';
+
+</script>
+
+<main>
+  <!-- <WidgetAdEngine 
+    authorId={[1]}
+  /> -->
+
+  <WidgetAdEngine 
+    authorArticleTagIds={[1]}
+  />
+</main>
